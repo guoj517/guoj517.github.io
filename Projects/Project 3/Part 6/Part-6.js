b36ef332f2f5588fc6c8549db701d867ea9e28ba
Amanda <amanda@Amanda-Guos-Mac.local>
2022-04-14 09:05:56 -0400

    update

--- Projects/Project 3/Part 6/Part-6.js
+++ Projects/Project 3/Part 6/Part-6.js
@@ -1,10 +1,10 @@
 const airtableApiKey = "keye0hFqnGOPHFLzw"
 
-const airtableDatabaseUrl = "https://api.airtable.com/v0/appTJh4SNTi7XOAW3/Project%203"
+const airtableDatabaseUrl = "https://api.airtable.com/v0/appTJh4SNTi7XOAW3/Project-3-Partner"
 
 const authenticatedUrl = airtableDatabaseUrl + "?api_key=" + airtableApiKey
 
-const illustrationContainerElement = document.querySelector('#assets')
+const illustrationContainerElement = document.querySelector('#illustrations')
 
 const fetchPromise = fetch(authenticatedUrl)
 const jsonPromise = fetchPromise.then ((response) => {
@@ -34,18 +34,4 @@ jsonPromise.then ((data)=> {
         containerElement.appendChild(titleElement)
     }
 })
-
-const 
-
-if (images.class !== undefined) {
-    classes = images.class;
-  }
-
-  var left = Math.round(Math.random() * 97) + -3;
-  var top = Math.round(Math.random() * 97) + -7;
-
-  var size = Math.round(Math.random() * 270) + 100;
-
-  var index = Math.round(Math.random() * 300) + 1;
-
-  var rotation = Math.round(Math.random() * 360) + 1;
+ 
