e1e46628680b295a8308395a27c36747b55bff00
Amanda <amanda@Amanda-Guos-Mac.local>
2022-04-21 16:11:20 -0400

    update

--- Assigments/a7.js
+++ Assigments/a7.js
@@ -14,6 +14,10 @@ const applicationElement = document.querySelector('#application')
 const colorElement = document.querySelector('#color')
 const adjElement = document.querySelector('#adj')
 const timeElement = document.querySelector('#time')
+const line1Element = document.querySelector('#line1')
+const line2Element = document.querySelector('#line2')
+const line3Element = document.querySelector('#line3')
+
 
 
 // SECTION 3: Application
@@ -37,12 +41,25 @@ fetch(authenticatedUrl)
     const adjs = words.filter((word) => {
         return word.fields.catagory === 'adj'
     })
+    const line1s = words.filter((word) => {
+      return word.fields.catagory === 'line1'
+  })
+  const line2s = words.filter((word) => {
+    return word.fields.catagory === 'line2'
+})
+  const line3s = words.filter((word) => {
+    return word.fields.catagory === 'line3'
+  })
 
 
     // Set Values onto HTML Elements 
     colorElement.innerHTML = colors[Math.floor(Math.random()*colors.length)].fields.text
     adjElement.innerHTML = adjs[Math.floor(Math.random()*adjs.length)].fields.text
     timeElement.innerHTML = times[Math.floor(Math.random()*times.length)].fields.text
+    line1Element.innerHTML = line1s[Math.floor(Math.random()*times.length)].fields.text
+    line2Element.innerHTML = line2s[Math.floor(Math.random()*times.length)].fields.text
+    line3Element.innerHTML = line3s[Math.floor(Math.random()*times.length)].fields.text
+    
     applicationElement.classList.add('loaded')
   })
   .catch((error) => {
