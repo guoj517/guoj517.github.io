10995eee31a95dc4ea3748f85f97bc933f83dbdd
Amanda <amanda@Amanda-Guos-Mac.local>
2022-05-13 10:44:56 -0400

    test

---
+++ Test.js
@@ -0,0 +1,18 @@
+document.getElementById("colored-element").style.color = "#ff0000";
+
+const childElement = document.createElement('p')
+    childElement.innerHTML = "Let's go with the dough!"
+    document.getElementById("parent-element").appendChild(childElement);
+
+
+const forLoopArray = ["Item One", "Item Two", "Item Three", "Item Four"]
+for (let i = 0; i < forLoopArray.length; i++) {
+    console.log(forLoopArray[i])
+  }
+
+const clickEventElement = document.querySelector('#event-listener-button')
+clickEventElement.addEventListener('click', (event) => {
+    console.log('click event', event)
+})
+
+
